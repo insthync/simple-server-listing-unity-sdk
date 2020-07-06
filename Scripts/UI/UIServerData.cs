ffc74018f34a431563e3152f84df0f084911f23d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-07-06 22:22:55 +0700

    Add UI scripts

---
+++ Scripts/UI/UIServerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SimpleServerListingSDK.UI
+{
+    public class UIServerData : MonoBehaviour
+    {
+        public ServerData serverData;
+        public Text textTitle;
+        public Text textDescription;
+        public Text textMap;
+        public string formatPlayer = "{0}/{1}";
+        public Text textPlayer;
+
+        private void Update()
+        {
+            if (textTitle) textTitle.text = serverData.title;
+            if (textDescription) textDescription.text = serverData.description;
+            if (textMap) textMap.text = serverData.map;
+            if (textPlayer) textPlayer.text = string.Format(formatPlayer, serverData.currentPlayer, serverData.maxPlayer);
+        }
+    }
+}
---
+++ Scripts/UI/UIServerList.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleServerListingSDK.UI
+{
+    public class UIServerList : MonoBehaviour
+    {
+        public Transform container;
+        public UIServerData uiPrefab;
+        public float updateInterval = 1f;
+        private float intervalCountDown;
+
+        private void Update()
+        {
+            if (intervalCountDown > 0)
+                intervalCountDown -= Time.deltaTime;
+            if (intervalCountDown <= 0)
+            {
+                intervalCountDown = updateInterval;
+                GetList();
+            }
+        }
+
+        public async void GetList()
+        {
+            var list = await SimpleServerListingSDK.Instance.List();
+            for (var i = container.childCount - 1; i >= 0; --i)
+            {
+                Destroy(container.GetChild(i).gameObject);
+            }
+            foreach (var data in list)
+            {
+                var newUI = Instantiate(uiPrefab, container);
+                newUI.serverData = data;
+            }
+        }
+    }
+}
